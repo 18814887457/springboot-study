e0bfd440328c2453028d6b629bb1ea2901c6db72
郑王龙 <1046104497@qq.com>
2020-01-17 14:16:17 +0800

    first commit

---
+++ study-exception/src/main/java/com/zola/studyexception/StudyExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.zola.studyexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyExceptionApplication.class, args);
+    }
+
+}
---
+++ study-exception/src/test/java/com/zola/studyexception/StudyExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zola.studyexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
